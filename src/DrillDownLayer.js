3fb8b1e414cb0e0df6c2c588c8a010209db66138
r21ling <r21lingx@gmail.com>
2021-09-10 18:06:49 +0800

    1 file modified

--- src/DrillDownLayer.js
+++ src/DrillDownLayer.js
@@ -6,15 +6,21 @@ import * as echarts from 'echarts';
 const DrillDownLayer = ({ onChage }) => {
   const containerRef = useRef(null);
   const chartRef = useRef(null);
-  const geoDataRef = useRef([]);
+  const prevAreaRef = useRef();
+  const currentAreaRef = useRef();
 
-  const drawMap = ({ level = 'country', target = '中国' } = {}) => {
+  const drawMap = ({
+    level = 'country',
+    target = '中国',
+    method = 'down'
+  } = {}) => {
     const district = new AMap.DistrictSearch({
       showbiz: false,
       subdistrict: 0, //获取边界不需要返回下级行政区
       extensions: 'base',
       level
     });
+
     district.search(target, function(status, result) {
       if (status !== 'complete') {
         return;
@@ -29,21 +35,69 @@ const DrillDownLayer = ({ onChage }) => {
               console.error(error);
               return;
             }
-            const subFeatures = areaNode.getSubFeatures();
-            console.log(subFeatures);
+            const features = areaNode.getSubFeatures();
+            console.log(method, target, prevAreaRef.current?.name, features);
             const mapJSON = {
               type: 'FeatureCollection',
-              features: subFeatures
+              features
             };
             echarts.registerMap(target, mapJSON);
-            const geoData = subFeatures.map(item => item.properties);
+            const geoData = features.map(item => item.properties);
 
             chartRef.current.setOption({
               // tooltip: {
               //   trigger: 'item'
               // },
+              geo: {
+                show: false,
+                map: target
+              },
               series: [
+                // {
+                //   name: 'Top5',
+                //   type: 'effectScatter',
+                //   coordinateSystem: 'geo',
+                //   data: [
+                //     {
+                //       name: '广东省',
+                //       value: 20
+                //     }
+                //   ],
+                //   symbolSize: 20,
+                //   showEffectOn: 'emphasis',
+                //   rippleEffect: {
+                //     brushType: 'stroke'
+                //   },
+                //   label: {
+                //     formatter: '{b}',
+                //     position: 'right',
+                //     show: true
+                //   },
+                //   itemStyle: {
+                //     color: '#f4e925',
+                //     shadowBlur: 10,
+                //     shadowColor: '#333'
+                //   },
+                //   zlevel: 1
+                // },
                 {
+                  type: 'effectScatter', //点
+                  coordinateSystem: 'geo',
+                  showEffectOn: 'render',
+                  zlevel: 1,
+                  rippleEffect: { period: 1, scale: 4, brushType: 'fill' },
+                  itemStyle: {
+                    normal: {
+                      color: '#1DE9B6',
+                      shadowBlur: 10,
+                      shadowColor: '#333'
+                    }
+                  },
+                  symbolSize: 5,
+                  data: [{ value: [118.8062, 31.9208] }]
+                },
+                {
+                  zoom: 1,
                   name: 'amap',
                   type: 'map',
                   // roam: true,
@@ -62,21 +116,21 @@ const DrillDownLayer = ({ onChage }) => {
                     // position: ['10%', '50%']
                     fontSize: 8
                   },
-                  emphasis: {
-                    itemStyle: {
-                      borderColor: '#25A8F6',
-                      borderWidth: 1,
-                      areaColor: 'transparent',
-                      shadowColor: 'rgba(0, 0, 0, 0)',
-                      shadowBlur: 0,
-                      shadowOffsetX: 0,
-                      shadowOffsetY: 1
-                    },
-                    label: {
-                      show: true,
-                      color: '#fff'
-                    }
-                  },
+                  // emphasis: {
+                  //   itemStyle: {
+                  //     borderColor: '#25A8F6',
+                  //     borderWidth: 1,
+                  //     areaColor: 'transparent',
+                  //     shadowColor: 'rgba(0, 0, 0, 0)',
+                  //     shadowBlur: 0,
+                  //     shadowOffsetX: 0,
+                  //     shadowOffsetY: 1
+                  //   },
+                  //   label: {
+                  //     show: true,
+                  //     color: '#fff'
+                  //   }
+                  // },
                   data: geoData
                 }
               ]
@@ -88,6 +142,14 @@ const DrillDownLayer = ({ onChage }) => {
   };
 
   useEffect(() => {
+    chartRef.current = echarts.init(containerRef.current);
+    chartRef.current.getZr().on('dblclick', params => {
+      drawMap({
+        method: 'up',
+        level: prevAreaRef.current.level,
+        target: prevAreaRef.current.name
+      });
+    });
     AMapLoader.load({
       key: 'dda3607ed8d2e71b44f3f96f1f3463ce', // 申请好的Web端开发者Key，首次调用 load 时必填
       version: '1.4.15', // 指定要加载的 JSAPI 的版本，缺省时默认为 1.4.15
@@ -99,10 +161,10 @@ const DrillDownLayer = ({ onChage }) => {
       }
     })
       .then(() => {
-        chartRef.current = echarts.init(containerRef.current);
         chartRef.current.on('click', { seriesName: 'amap' }, params => {
+          // chartRef.current.on('click', 'geo', params => {
           const { data } = params;
-          if (data.level === 'district') {
+          if (data?.level === 'district') {
             return;
           }
           drawMap({
@@ -111,10 +173,7 @@ const DrillDownLayer = ({ onChage }) => {
           });
         });
 
-        drawMap({
-          level: 'province',
-          target: '新疆'
-        });
+        drawMap();
       })
       .catch(e => {
         console.error(e);
